0d5354acb046cfffd7a1529105ea7891500f9f79
Foo Ren Xiang <ren_xiang_foo@epam.com>
2020-07-10 21:46:29 +0800

    changed bind target

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-docker run -d -t -p 1234:1234 --name localdev --volume //c/Users/Ren_Xiang_Foo/dockershare:/home/foorx/dockershare foorenxiang/esdev
+docker run -d -t -p 1234:1234 --name localdev --volume //c/Users/Ren_Xiang_Foo/dockershare:/root/dockershare foorenxiang/esdev
